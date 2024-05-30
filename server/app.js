9f7f6ce96198bae413fed9b6c879bb97e2ed473d
ilyesmehri <mehri.ilyes20@gmail.com>
2024-05-30 13:08:05 +0100

    tawa2

--- server/app.js
+++ server/app.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 const db = require("./database/index");
 const router = require("./routes/productsRouts");
 const routerAuth = require("./routes/authRoutes");
+const routerusers=require("./routes/usersRoutes")
+const routerPa = require("./routes/PanierRoutes.js");
 const PORT = 4000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", router);
 app.use("/api/auth", routerAuth);
+app.use("/api/users",routerusers)
+app.use("/api/panier", routerPa);
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
